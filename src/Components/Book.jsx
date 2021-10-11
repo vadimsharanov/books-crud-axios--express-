99a2b01e628fcea2d141ab30058b0a0f7898346e
Vadim Šaranov <vadim.sharanov@gmail.com>
2021-10-11 11:06:00 +0300

    main things done

---
+++ src/Components/Book.jsx
@@ -0,0 +1,9 @@
+function Book({data}) {
+    return (
+        <div>
+            there will be some book
+        </div>
+    )
+}
+
+export default Book
--- src/Components/Books.jsx
+++ src/Components/Books.jsx
@@ -1,10 +1,24 @@
-
+import { useEffect, useState } from "react"
+import Book from "./Book";
+import axios from "axios"
 function Books() {
     
+    const [books, setBooks] = useState([]);
+
+
+    useEffect(() => {
+        axios.get("http://localhost:4000/books")
+        .then((res)=> {
+            setBooks(res.data)
+        })
+    }, [])
+
+
+
     return (
 
-        <div className="main-container">
-        Books
+        <div className="row">
+        {books.map((item)=>  <Book data={item} key={item.id} ></Book> )}
         </div>
     )
 }
